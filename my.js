a36346c39e62bfa5e287d329acca834e5f7240eb
leelaplap <letandat27101998@gmail.com>
2020-02-07 17:57:25 +0700

    question and answer

--- my.js
+++ my.js
@@ -1,5 +1,4 @@
 function showMenu() {
-
     var x = document.getElementById('menu');
     if (x.classList.contains('hide_menu')) {
         x.classList.add('show_menu');
@@ -7,5 +6,51 @@ function showMenu() {
     } else {
         x.classList.add('hide_menu');
         x.classList.remove('show_menu');
+    }
+}
+
+function showAnswer1() {
+    var x = document.getElementById('answer1');
+    if (x.classList.contains('hide_answer')) {
+        x.classList.add('show_answer');
+        x.classList.remove('hide_answer');
+    } else {
+        x.classList.add('hide_answer');
+        x.classList.remove('show_answer');
+    }
+
+
+}
+
+function showAnswer2() {
+    var x = document.getElementById('answer2');
+    if (x.classList.contains('hide_answer')) {
+        x.classList.add('show_answer');
+        x.classList.remove('hide_answer');
+    } else {
+        x.classList.add('hide_answer');
+        x.classList.remove('show_answer');
+    }
+}
+
+function showAnswer3() {
+    var x = document.getElementById('answer3');
+    if (x.classList.contains('hide_answer')) {
+        x.classList.add('show_answer');
+        x.classList.remove('hide_answer');
+    } else {
+        x.classList.add('hide_answer');
+        x.classList.remove('show_answer');
+    }
+}
+
+function showAnswer4() {
+    var x = document.getElementById('answer4');
+    if (x.classList.contains('hide_answer')) {
+        x.classList.add('show_answer');
+        x.classList.remove('hide_answer');
+    } else {
+        x.classList.add('hide_answer');
+        x.classList.remove('show_answer');
     }
 }
